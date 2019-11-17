c3b1331721c07dedc402c5c9f24b0f3d687b0aef
Ravi Raju <toravir@yahoo.com>
2019-11-16 23:14:02 -0800

    binary log first test
    Signed-off-by: Ravi Raju <toravir@yahoo.com>

--- tests/check_logger.c
+++ tests/check_logger.c
@@ -14,6 +14,16 @@ const char *readLineFromFile(const char *fName)
   return logLine;
 }
 
+const char *readNBytes(FILE *fp, int n)
+{
+  char *buf = calloc(n, 1);
+  if (!buf) {
+      return NULL;
+  }
+  fread(buf, n, 1, fp);
+  return buf;
+}
+
 START_TEST(test_logger_create) {
   logHandle *m;
   char tmpFile[] = "/tmp/libczTest_XXXXXX";
@@ -225,6 +235,29 @@ START_TEST(test_logger_context) {
   unlink(tmpFile);
 } END_TEST
 
+START_TEST(test_bin_logger_create) {
+  logHandle *m;
+  char tmpFile[] = "/tmp/libczTest_XXXXXX";
+  m = newlogHandleFd(mkstemp(tmpFile), LOG_DEBUG, 1);
+  ck_assert_ptr_ne(m, NULL);
+  setLogAutoTs(m, FALSE);
+  doLog(m, LOG_WARN, L_INT("price", 1000), L_STR("Desc", "Without TS"), L_PRINT);
+  FILE *rdback = fopen(tmpFile, "rb");
+  const char expected[] = {
+         0xbf, 0x65, 0x70, 0x72, 0x69, 0x63, 0x65, 0x19, 0x03, 0xe8, 0x64, 0x44, 0x65, 0x73, 0x63, 0x6a,
+         0x57, 0x69, 0x74, 0x68, 0x6f, 0x75, 0x74, 0x20, 0x54, 0x53, 0x65, 0x6c, 0x65, 0x76, 0x65, 0x6c,
+         0x64, 0x77, 0x61, 0x72, 0x6e, 0xff,
+  };
+  const char *bytes = readNBytes(rdback, sizeof(expected));
+  for (int i = 0; i < sizeof(expected); i++)
+      ck_assert_msg(bytes[i] == expected[i],
+              "At index: %d, expected: 0x%02X, got 0x%02X", i, expected[i], bytes[i]);
+
+  free((void*)bytes);
+  unlink(tmpFile);
+} END_TEST
+
+
 
 Suite *logger_suite(void) {
   Suite *s;
@@ -239,6 +272,8 @@ Suite *logger_suite(void) {
   tcase_add_test(tc_core, test_logger_multiple_call);
   tcase_add_test(tc_core, test_logger_levels);
   tcase_add_test(tc_core, test_logger_context);
+
+  tcase_add_test(tc_core, test_bin_logger_create);
   suite_add_tcase(s, tc_core);
 
   return s;
